ec23c900020203653847cde5e7c0fe41147881cd
cammip <cammip@uw.edu>
2025-04-15 19:50:20 -0700

    created roadmap file

---
+++ client/src/screens/roadmap.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Roadmap = () => {
+    return (
+        <>
+        <div>
+            <p>Roadmap</p>
+        </div>
+        </>
+    )
+}
+
+export default Roadmap

e6e64b07759c5e45bcb84b2af8417b0352746afa
cammip <cammip@uw.edu>
2025-04-15 22:11:04 -0700

    worked on roadmap

--- client/src/screens/roadmap.jsx
+++ client/src/screens/roadmap.jsx
@@ -3,8 +3,42 @@ import React from 'react'
 const Roadmap = () => {
     return (
         <>
-        <div>
-            <p>Roadmap</p>
+        <p className='backBtn'>&lt; Back</p>
+        
+        <div className='flexContainer'>
+            <div className='item1'>
+                <div className='moduleInfo'>
+                    <div className='moduleContent'>
+                        <p className='homeHeading'><b>Module name</b></p>
+                        <p>Build a professional resume step-by-step with our guided tool.</p>
+                        <div className='moduleStatusDiv'>
+                            <p>Status</p>
+                        </div>
+                        <p className='taskCount'>📘 <span className='numArticles'>5</span> articles</p>
+                    </div>
+                    
+                </div>
+
+                <div className='nextTask'>
+                    <div>
+                        <p>Current task name</p>
+                        <p>Quick description</p>
+                        
+                        <div className='moduleButtonDiv'>
+                            <p>Start task</p>
+                        </div>
+                    </div>
+                    
+                </div>
+
+                <p>Dont know where to start? Take our quiz!</p>
+            </div>
+            
+            <div className='item2'></div>
+
+            <div className='item3'>
+                <p>Complete module</p>
+            </div>
         </div>
         </>
     )

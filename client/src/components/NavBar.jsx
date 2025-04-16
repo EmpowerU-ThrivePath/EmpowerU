106bc9af9b20418b7af64a93ef70115e88885100
lindsym <lindsym@uw.edu>
2025-04-16 11:45:16 -0700

    idk

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const NavBar = () => {
     return (
         <>
         <div className="navbar">
             <div className="navbar-left">
-                <h2>ThrivePath</h2>
+                <img src='\ThrivePath.png' className='logo' alt='ThrivePath'></img>
+            </div>
+            <div className='navbar-center'></div>
+            <div className='navbar-right'>
+                <Link to={"./Home"} className='tab'>Dashboard</Link>
+                <Link to={"./Home"} className='tab'>Next Step Navigator</Link>
+                <img src="\Avatar 5.png" className='pfp' alt='profile picture'></img>
             </div>
         </div>
-        <hr></hr>
         </>
     )
 }

65dc4002d4d1a3ee1bbad5318f1a7450d3f83d16
cammip <cammip@uw.edu>
2025-04-17 21:36:25 -0700

    roadmap updates

--- client/src/screens/home.jsx
+++ client/src/screens/home.jsx
@@ -3,38 +3,44 @@ import React from 'react'
 const Home = () => {
     return (
         <>
-        <div className='homeHeader'>
-            <p className='homeHeading'><b>Good morning, <span>User!</span></b></p>
-            <p className='homeText'>Let’s begin working on your journey.</p>
+        <div className='home-header'>
+            <p className='home-heading'><b>Good morning, <span>User!</span></b></p>
+            <p className='home-text'>Let’s begin working on your journey.</p>
         </div>
 
-        <div className='allModulesDiv'>
+        <div className='all-modules-div'>
             <div className='module'>
-                <div className='moduleContent'>
-                    <p className='moduleName'><b>Resume</b></p>
-                    <div className='moduleStatusDiv'>
+                <div className='module-content'>
+                    <p className='module-name'><b>Resume</b></p>
+                    <div className='module-status-div'>
                         <p>In progress</p>
                     </div>
-                    <div className='moduleButtonDiv'>
-                        <p>Continue</p>
+                    <div className='home-button-div'>
+                        <div className='module-button-div'>
+                            <p>Continue</p>
+                        </div>
                     </div>
+                    
                 </div>
-                <div className='moduleImg'>
+                <div className='module-img'>
                     <img src='https://media.istockphoto.com/id/1304527612/vector/resumes-cv-application-resume-filling-concept-writing-business-resume-job-search-resume.jpg?s=612x612&w=0&k=20&c=wKhHHikcoQJ1pEGaOgSUWsRoXEYr1TTbvbHcrpm1zGo='></img>
                 </div>
             </div>
 
             <div className='module'>
-                <div className='moduleContent'>
-                    <p className='moduleName'><b>Module Name</b></p>
-                    <div className='moduleStatusDiv'>
+                <div className='module-content'>
+                    <p className='module-name'><b>Module Name</b></p>
+                    <div className='module-status-div'>
                         <p>User's Status</p>
                     </div>
-                    <div className='moduleButtonDiv'>
-                        <p>Continue</p>
+                    <div className='home-button-div'>
+                        <div className='module-button-div'>
+                            <p>Continue</p>
+                        </div>
                     </div>
+                    
                 </div>
-                <div className='moduleImg'>
+                <div className='module-img'>
                     <img src='https://png.pngtree.com/element_our/20190530/ourmid/pngtree-white-spot-float-image_1256405.jpg'></img>
                 </div>
             </div>
--- client/src/screens/roadmap.jsx
+++ client/src/screens/roadmap.jsx
@@ -1,32 +1,38 @@
 import React from 'react'
 
+// do tmrw: create standard className for text size only
+// redo className naming convention to "dash-separated"
+
 const Roadmap = () => {
     return (
         <>
-        <p className='backBtn'>&lt; Back</p>
+        <p className='back-btn'>&lt; Back</p>
         
-        <div className='flexContainer'>
+        <div className='flex-container'>
             <div className='item1'>
-                <div className='moduleInfo'>
-                    <div className='moduleContent'>
-                        <p className='homeHeading'><b>Module name</b></p>
+                <div className='module-info'>
+                    <div className='module-content'>
+                        <p className='home-heading'><b>Module name</b></p>
                         <p>Build a professional resume step-by-step with our guided tool.</p>
-                        <div className='moduleStatusDiv'>
-                            <p>Status</p>
+                        <div className='module-status-div'>
+                            <p>In progress</p>
                         </div>
-                        <p className='taskCount'>📘 <span className='numArticles'>5</span> articles</p>
+                        <p className='task-count'>📓 <span className='num-articles'>5</span> articles</p>
                     </div>
                     
                 </div>
 
-                <div className='nextTask'>
-                    <div>
-                        <p>Current task name</p>
-                        <p>Quick description</p>
+                <div className='next-task'>
+                    <div className='next-task-content'>
+                        <p className='module-name'><b>Add name & personal info</b></p>
+                        <p>Include your full name, phone number, email address, and additional links</p>
                         
-                        <div className='moduleButtonDiv'>
-                            <p>Start task</p>
+                        <div className='next-task-button-div'>
+                            <div className='module-button-div'>
+                                <p>Start task</p>
+                            </div>
                         </div>
+                        
                     </div>
                     
                 </div>
@@ -34,7 +40,29 @@ const Roadmap = () => {
                 <p>Dont know where to start? Take our quiz!</p>
             </div>
             
-            <div className='item2'></div>
+            <div className='roadmap-steps'>
+                <div className='step'>
+                    <div className='step-bubble green'>
+                        <img className='bubble-img' src='https://www.freeiconspng.com/thumbs/white-arrow-png/white-arrow-image-png-14.png'></img>
+                    </div>
+                    <p>Add name and personal information</p>
+                </div>
+
+                <div className='step'>
+                    <div className='step-bubble'></div>
+                    <p>Add education</p>
+                </div>
+
+                <div className='step'>
+                    <div className='step-bubble'></div>
+                    <p>List work experience/projects</p>
+                </div>
+
+                <div className='step'>
+                    <div className='step-bubble'></div>
+                    <p>Add skills</p>
+                </div>
+            </div>
 
             <div className='item3'>
                 <p>Complete module</p>

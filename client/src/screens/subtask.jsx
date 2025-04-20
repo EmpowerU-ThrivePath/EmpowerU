6827edffaf7491dcb1c80ec5717bdae2b2ebf88a
cammip <cammip@uw.edu>
2025-04-19 20:39:40 -0700

    done subtask frontend

--- client/src/screens/subtask.jsx
+++ client/src/screens/subtask.jsx
@@ -7,6 +7,32 @@ const Subtask = () => {
             <p className='back-btn'>&lt; Back</p>
             
             <div className='progress-bar'>
+                <div className='progress-circle-div'>
+                    <div className='progress-circle progress-circle-inprogress'>
+                        <img className='inprogress-dot' src='https://onedaycolor.com/cdn/shop/products/GPCX4030_650x.png?v=1533739257'></img>
+                    </div>
+                    <p className='progress-name'>Personal information</p>
+                </div>
+
+                <div className='progress-circle-div'>
+                    <div className='progress-circle progress-circle-incomplete'></div>
+                    <p className='progress-name'>Education</p>
+                </div>
+
+                <div className='progress-circle-div'>
+                    <div className='progress-circle progress-circle-incomplete'></div>
+                    <p className='progress-name'>Experience</p>
+                </div>
+
+                <div className='progress-circle-div'>
+                    <div className='progress-circle progress-circle-incomplete'></div>
+                    <p className='progress-name'>Skills</p>
+                </div>
+
+                <div className='progress-circle-div'>
+                    <div className='progress-circle progress-circle-incomplete'></div>
+                    <p className='progress-name'>Proofread</p>
+                </div>
             </div>
 
             <div className='subtask-content'>
@@ -25,22 +51,23 @@ const Subtask = () => {
                 </p>
             </div>
 
-            <p><b>Example</b></p>
-            <div className='example-div'></div>
+           <p className='thirty-px'><b>Example</b></p>
+            <div className='example-div'>
+                <img className='example-img' src='https://www.cvplaza.com/wp-content/uploads/2012/09/cv-personal-profile-statement.png'></img>
+            </div>
 
-            <p><b>Tips for succes</b>s</p>
-
+            <p className='thirty-px'><b>Tips for success</b></p>
             <div className='all-tips'>
                 <div className='tip'>
-                    <img className='tip-img' src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/960px-LinkedIn_logo_initials.png'></img>
-                    <p><b>Include your LinkedIn/portfolio</b></p>
-                    <p>Add links to your LinkedIn or portfolio to showcase your work</p>
+                    <img className='tip-img' src='https://www.designyup.com/wp-content/uploads/2019/03/arial-font-sample.jpg'></img>
+                    <p><b>Use a clear, readable font</b></p>
+                    <p>Choose a professional font to ensure your name and contact info are easy to read</p>
                 </div>
 
                 <div className='tip'>
-                    <img className='tip-img' src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/960px-LinkedIn_logo_initials.png'></img>
-                    <p><b>Include your LinkedIn/portfolio</b></p>
-                    <p>Add links to your LinkedIn or portfolio to showcase your work</p>
+                    <img className='tip-img' src='https://pictarts.com/07/material/01-free/0054-clip-art-m.png'></img>
+                    <p><b>Have a professional email</b></p>
+                    <p>Use a simple, name-based email instead of a casual or outdated address</p>
                 </div>
 
                 <div className='tip'>
@@ -50,7 +77,7 @@ const Subtask = () => {
                 </div>
             </div>
            
-           <p><b>Resources</b></p>
+           <p className='thirty-px'><b>Resources</b></p>
            <div className='resource-div'>
                 <div className='resource'>
                     <p>Free Resume Examples and Samples for 2025</p>

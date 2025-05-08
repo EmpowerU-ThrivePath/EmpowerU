21494a1b24cd57e44b551a70879250bd2987ce5a
Paul Oh <poh2@uw.edu>
2025-05-08 14:59:08 -0700

    fix models layout

--- server/models.js
+++ server/models.js
@@ -1,8 +1,7 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 const { Schema } = mongoose;
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 const SALT_WORK_FACTOR = 10;
-
 
 let models = {};
 console.log("connecting to mongodb");
@@ -12,62 +11,52 @@ await mongoose.connect(
 console.log("successfully connected to mongodb!");
 
 const profileSchema = new mongoose.Schema({
-    fname: String,
-    lname: String,
-    pronouns: String,
-    email: String,
-    grad_year: String,
-    intended_career: String,
-    password: { type: String, required: true },
-    signed_in: Boolean
-})
-
-     
-profileSchema.pre('save', function(next) {
-    var user = this;
-
-    // only hash the password if it has been modified (or is new)
-    if (!user.isModified('password')) return next();
-
-    // generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-        if (err) return next(err);
-
-        // hash the password using our new salt
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            if (err) return next(err);
-            // override the cleartext password with the hashed one
-            user.password = hash;
-            next();
-        });
-    });
-});
-     
-profileSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
-    });
-};
-
-const messageSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    message: String
-})
-
-models.Message = mongoose.model('Message', messageSchema)
-models.Profile = mongoose.model('Profile', profileSchema)
-console.log("mongoose models created")
-
   fname: String,
   lname: String,
   pronouns: String,
   email: String,
   grad_year: String,
   intended_career: String,
+  password: { type: String, required: true },
+  signed_in: Boolean,
 });
+
+profileSchema.pre("save", function (next) {
+  var user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified("password")) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+profileSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+const messageSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  message: String,
+});
+
+models.Message = mongoose.model("Message", messageSchema);
 models.Profile = mongoose.model("Profile", profileSchema);
+console.log("mongoose models created");
 
 const optionSchema = new mongoose.Schema({
   label: String,
